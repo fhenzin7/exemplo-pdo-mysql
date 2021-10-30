be0723dddd736287ce9629d71527f45c499bac7f
fhenzin7 <faustohenrique7645@gmail.com>
2021-10-30 20:29:30 +0000

    includes

--- delete.php
+++ delete.php
@@ -18,18 +18,11 @@
         header('Location:/index.php');
     }
 
+$_title = 'Deletar Gênero';
 
 ?>
+<?php include('./includes/header.php') ?>
 
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
-    <title>Excluir Gênero</title>
-</head>
-<body>
-    <main class="container">
         <h1>Deletar</h1>
         <p>Tem certeza que quer excluir o gênero <?= $genero['nome'] ?> ?</p>
         
@@ -38,6 +31,4 @@
             <a class="btn btn-secondary" href="index.php">Voltar</a>
             <button class="btn btn-danger"type="submit">Excluir</button>
         </form>
-    </main>
-</body>
-</html> 
+<?php include('./includes/footer.php') ?>
--- index.php
+++ index.php
@@ -9,18 +9,12 @@ $comando = $bd->prepare('SELECT * FROM generos');
 $comando->execute();
 $generos = $comando->fetchAll(PDO::FETCH_ASSOC);
 
+$_title = 'Gêneros';
+
 ?>
 
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+<?php include('./includes/header.php') ?>
 
-    <title>Biblioteca</title>
-</head>
-    <body>
-        <main class="container">
             <a class="btn btn-primary" href="insert.php">Novo Gênero</a>
                 <table class="table">
                     <tr>
@@ -39,6 +33,4 @@ $generos = $comando->fetchAll(PDO::FETCH_ASSOC);
                         </tr>
                         <?php endforeach ?>
                 </table>
-            </main>
-    </body>
-</html>
+    <?php include('./includes/footer.php') ?>
