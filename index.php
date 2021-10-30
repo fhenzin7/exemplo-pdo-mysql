88e98ed7b65f69e5e13370c3f61bdae412552039
fhenzin7 <faustohenrique7645@gmail.com>
2021-10-30 19:10:24 +0000

    bootstrap

--- index.php
+++ index.php
@@ -15,26 +15,30 @@ $generos = $comando->fetchAll(PDO::FETCH_ASSOC);
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+
     <title>Biblioteca</title>
 </head>
     <body>
-        <a href="insert.php">Novo Gênero</a>
-        <table>
-            <tr>
-                <th>id</th>
-                <th>Nome</th>
-                <th>&nbsp;</th>
-            </tr>
-            <?php foreach($generos as $g): ?>
-                <tr>
-                    <td><?=$g['id']?></td>
-                    <td><?=$g['nome']?></td>
-                    <td>
-                        <a href="update.php?id=<?= $g['id'] ?>">Editar</a>
-                        <a href="delete.php?id=<?=$g['id'] ?>">Remover</a>
-                    </td>
-                </tr>
-                <?php endforeach ?>
-        </table>
+        <main class="container">
+            <a class="btn btn-primary" href="insert.php">Novo Gênero</a>
+                <table class="table">
+                    <tr>
+                        <th>id</th>
+                        <th>Nome</th>
+                        <th>&nbsp;</th>
+                    </tr>
+                    <?php foreach($generos as $g): ?>
+                        <tr>
+                            <td><?=$g['id']?></td>
+                            <td><?=$g['nome']?></td>
+                            <td>
+                                <a class="btn btn-secondary" href="update.php?id=<?= $g['id'] ?>">Editar</a>
+                                <a class="btn btn-danger" href="delete.php?id=<?=$g['id'] ?>">Remover</a>
+                            </td>
+                        </tr>
+                        <?php endforeach ?>
+                </table>
+            </main>
     </body>
 </html>
--- update.php
+++ update.php
@@ -25,15 +25,22 @@ if($_SERVER['REQUEST_METHOD'] == 'GET') {
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     <title>Editar Gênero</title>
 </head>
 <body>
+    <main class="container">
     <h1>Editar Gênero</h1>
     <form action="update.php" method="post">
         <input type="hidden" name="id" value="<?= $genero['id'] ?> "/>
-        <label for="nome">Nome do Gênero</label>
-        <input type="text" required name="nome" value="<?= $genero['nome']?>"/>
-        <button type="submit">Salvar</button>
+        <div class="form-group">
+        <label for="nome">Nome</label>
+        <input class="form-control" type="text" required name="nome" value="<?= $genero['nome']?>"/>
+        </div>
+        <br>
+        <a class="btn btn-secondary"  href="index.php">Voltar</a>
+        <button class="btn btn-success" type="submit">Salvar</button>
     </form>
+    </main>
 </body>
 </html>
